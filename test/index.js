c8f2ffd405c5adb1eb151760a40c57341c9cd1b8
Ryan Tsao <rtsao@uber.com>
2015-09-21 23:07:37 -0700

    Basic composition

--- test/index.js
+++ test/index.js
@@ -8,3 +8,18 @@ test('basic template string functionality', function t(assert) {
   assert.equal(result.css, 'foo', 'basic template string works');
   assert.end();
 });
+
+test('basic template string functionality', function t(assert) {
+  var result = csjs`
+
+    .foo {}
+    .bar extends .foo {}
+
+  `;
+
+  assert.ok(result, 'result exists');
+  assert.ok(result.extensions, 'extensions exist');
+  assert.ok(result.extensions.bar, 'bar has an extension');
+  assert.equal(result.extensions.bar.className, 'bar foo', 'bar extends foo');
+  assert.end();
+});
